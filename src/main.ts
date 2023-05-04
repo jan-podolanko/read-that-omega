7c43a66a080b08e81b4375e000df4bcd13059513
jan-podolanko <jan.podolanko11@gmail.com>
2023-05-04 19:35:51 +0200

    config moved

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from './firebase/config';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 createApp(App).mount('#app')
