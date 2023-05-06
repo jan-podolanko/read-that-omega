4c19d91ac5c670f2fdb26f32b4a4a3665eec1eb1
Bartosz Laskowski <laskowski.bartosz99@gmail.com>
2023-05-06 20:24:27 +0200

    Added comments section in PostScreen

--- src/ui/screens/PostScreen.vue
+++ src/ui/screens/PostScreen.vue
@@ -3,11 +3,25 @@
     import { usePostsStore } from "../../stores/posts";
     import { Post } from "../../model/Post";
     import { useDateFormat } from "@vueuse/core";
+    import { useRouter } from "vue-router";
+    import TextButton from "../components/TextButton.vue";
+    import { useToast } from "vue-toastification";
 
     const postsStore = usePostsStore();
+    const toast = useToast();
+    const router = useRouter();
 
-    const state: { posts: Post[] } = reactive({
+    const state: { posts: Post[], comments: Comment[] } = reactive({
         posts: [],
+        comments: [],
+    });
+
+    const comment: {
+        body: string;
+        id: string;
+    } = reactive({
+        body: "",
+        id: ""
     });
 
     onBeforeMount(() => {
@@ -15,6 +29,10 @@
             console.log(posts);
             state.posts = posts;
         });
+        postsStore.getPostComments(props.id).then(comments => {
+            console.log(comments);
+            state.comments = comments;
+        })
     });
 
     async function likePost(post: Post) {
@@ -31,11 +49,50 @@
         }
     }
 
+    async function likeComment(comment: Comment) {
+        if (comment.didUserLike) {
+            if (await postsStore.dislikeComment(comment.id)) {
+                comment.likeAmount--;
+                comment.didUserLike = false;
+            }
+        } else {
+            if (await postsStore.likeComment(comment.id)) {
+                comment.likeAmount++;
+                comment.didUserLike = true;
+            }
+        }
+    }
+
+    
+
+    function errorHandler(message: String, duration: number = 200) {
+        toast.error(message);
+        navigator.vibrate(duration);
+    }
+
+    async function createComment() {
+        const body = comment.body.trim();
+        const postid = props.id
+        if (body.length == 0) {
+            errorHandler("Comment cannot be empty");
+            return;
+        }
+        const isSuccess = await postsStore.createComment({
+            postid,
+            body,
+        });
+
+        if (isSuccess) {
+            router.go(0);
+        }
+
+    }
+
     const props = defineProps(['id'])
 
     const filteredPosts = computed(() => {
     return state.posts.filter(post => post.id === props.id);
-  });
+     });
 </script>
 
 
@@ -80,12 +137,70 @@
                 />
             </div>
         </router-link>
+
         </section>
+        <p>Comments:</p>
+
+        <section class="post" v-for="comment in state.comments" :key="comment.id">
+            <time :datetime="comment.date.toISOString()"
+                    >{{ useDateFormat(comment.date, "D.MM.YY").value }}<br />@
+                    {{ useDateFormat(comment.date, "HH:mm").value }}
+                </time>
+
+                <div class="post-body">
+                <p>{{ comment.body }}</p>
+            </div>
+
+
+            <div class="post-actions">
+                <span style="margin-bottom: 2px; margin-right: 3px">{{
+                    comment.likeAmount
+                }}</span>
+                <span class="material-icons" @click="likeComment(comment)">{{
+                    comment.didUserLike ? "favorite" : "favorite_outlined"
+                }}</span>
+                <div style="flex-grow: 1"></div>
+                <span class="post-author-username">{{
+                    comment.author.displayName
+                }}</span>
+                <img
+                    class="post-author-photo"
+                    :src="comment.author.photoURL"
+                    alt=""
+                />
+                </div>
+        </section>
+
     </main>
+    <form action="#" @submit.prevent="createComment()">
+        <textarea
+            placeholder="Add comment..."
+            maxlength="400"
+            v-model.trim="comment.body"
+        ></textarea>
+        <section class="button-row">
+            <TextButton>Post</TextButton>
+        </section>
+        
+    </form>
 </template>
 
 
 <style scoped lang="scss">
+    textarea {
+        background-color: $surfaceVariant;
+        border: none;
+        width: 100%;
+        height: 5rem;
+        resize: none;
+        padding: 0.7rem;
+        border: none;
+        border-radius: 24px;
+
+        &:focus {
+            outline: none;
+        }
+    }
     a {
         background-color: $surfaceVariant;
         color: rgba($onSurfaceVariant, 0.7);
@@ -178,4 +293,16 @@
             border-radius: 50%;
         }
     }
+
+    .button-row {
+        display: flex;
+        justify-content: flex-end;
+        margin-top: 1rem;
+    }
+
+    form {
+        padding: 0.5rem;
+    }
+
+    
 </style>
