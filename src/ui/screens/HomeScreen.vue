316c91cd0c985575c41b35c5b3e4213ef1f0c6bf
akrucan <andrzejkrucan@gmail.com>
2023-05-06 21:12:34 +0200

    Im gonna push

--- src/ui/screens/HomeScreen.vue
+++ src/ui/screens/HomeScreen.vue
@@ -1,220 +1,231 @@
 <script setup lang="ts">
-    import { onBeforeMount, reactive } from "vue";
-    import { usePostsStore } from "../../stores/posts";
-    import { Post } from "../../model/Post";
-    import { useDateFormat } from "@vueuse/core";
-    import { useUserStore } from "../../stores/user";
+import {onBeforeMount, reactive} from "vue";
+import {usePostsStore} from "../../stores/posts";
+import {Post} from "../../model/Post";
+import {useDateFormat} from "@vueuse/core";
+import {useUserStore} from "../../stores/user";
 
-    const postsStore = usePostsStore();
-    const userStore = useUserStore();
-    const state: { posts: Post[], subjects: Array<any> | null } = reactive({
-        posts: [],
-        subjects: [],
+const postsStore = usePostsStore();
+const userStore = useUserStore();
+const state: { posts: Post[], subjects: Array<any> | null } = reactive({
+    posts: [],
+    subjects: [],
+});
+const filter = ""
+onBeforeMount(() => {
+    postsStore.getPosts().then(posts => {
+        console.log(posts);
+        state.posts = posts;
     });
-    const filter= ""
-    onBeforeMount(() => {
-        postsStore.getPosts().then(posts => {
-            console.log(posts);
-            state.posts = posts;
-        });
-        userStore.getSubjects().then(subjects => {
-            console.log(subjects);
-            state.subjects = subjects;
-        });
+    userStore.getSubjects().then(subjects => {
+        console.log(subjects);
+        state.subjects = subjects;
     });
+});
 
 
-    async function likePost(post: Post) {
-        if (post.didUserLike) {
-            if (await postsStore.dislikePost(post.id)) {
-                post.likeAmount--;
-                post.didUserLike = false;
-            }
-        } else {
-            if (await postsStore.likePost(post.id)) {
-                post.likeAmount++;
-                post.didUserLike = true;
-            }
+async function likePost(post: Post) {
+    if (post.didUserLike) {
+        if (await postsStore.dislikePost(post.id)) {
+            post.likeAmount--;
+            post.didUserLike = false;
+        }
+    } else {
+        if (await postsStore.likePost(post.id)) {
+            post.likeAmount++;
+            post.didUserLike = true;
         }
     }
+}
 
-    function filterBySubject(subject: String){
-        return state.subjects.filter((post: { subject: String; }) => post.subject == subject)
-    }
+function filterBySubject(subject: String) {
+    return state.subjects.filter((post: { subject: String; }) => post.subject == subject)
+}
 </script>
 
 <template>
     <main id="homeScreen">
-    <div id="posts">
-        <select><option v-for="subject in state.subjects" style="color:black" :v-model="filter">{{ subject.subject }}</option></select><button @click="filterBySubject(filter)">filter by subject</button>
-        <section class="post" v-for="post in state.posts">
-            <router-link :to="{ name: 'postid', params: { id: post.id } }">
-            <header class="post-header">
-                <div>
-                    <span>{{ post.title }}</span> <br />
-                    <div v-if="post.location !== null" class="location-header">
-                        <span class="material-icons"> pin_drop </span>
-                        <span>{{ post.location }}</span>
-                    </div>
+        <div id="posts">
+            <select>
+                <option v-for="subject in state.subjects" style="color:black" :v-model="filter">{{
+                    subject.subject
+                    }}
+                </option>
+            </select>
+            <button @click="filterBySubject(filter)">filter by subject</button>
+            <section class="post" v-for="post in state.posts">
+                <router-link :to="{ name: 'postid', params: { id: post.id } }">
+                    <header class="post-header">
+                        <div>
+                            <span>{{ post.title }}</span> <br/>
+                            <div v-if="post.location !== null" class="location-header">
+                                <span class="material-icons"> pin_drop </span>
+                                <span>{{ post.location }}</span>
+                            </div>
+                        </div>
+                        <time :datetime="post.date.toISOString()"
+                        >{{ useDateFormat(post.date, "D.MM.YY").value }}<br/>@
+                            {{ useDateFormat(post.date, "HH:mm").value }}
+                        </time>
+                    </header>
+                </router-link>
+                <div class="post-body">
+                    <p>{{ post.body }}</p>
                 </div>
-                <time :datetime="post.date.toISOString()"
-                    >{{ useDateFormat(post.date, "D.MM.YY").value }}<br />@
-                    {{ useDateFormat(post.date, "HH:mm").value }}
-                </time>
-            </header>
-            </router-link>
-            <div class="post-body">
-                <p>{{ post.body }}</p>
-            </div>
-            <div v-if="post.imageURL !== null" class="post-image">
-                <img :src="`${post.imageURL}`" />
-            </div>
-            <div class="post-actions">
+                <div v-if="post.imageURL !== null" class="post-image">
+                    <img alt = "image post" :src="`${post.imageURL}`"/>
+                </div>
+                <div class="post-actions">
                 <span style="margin-bottom: 2px; margin-right: 3px">{{
-                        post.likeAmount
+                    post.likeAmount
                     }}</span>
                     <span class="material-icons" @click="likePost(post)">{{
-                            post.didUserLike ? "favorite" : "favorite_outlined"
+                        post.didUserLike ? "favorite" : "favorite_outlined"
                         }}</span>
                     <div style="flex-grow: 1"></div>
                     <span class="post-author-username">{{
-                            post.author.displayName
+                        post.author.displayName
                         }}</span>
                     <img
-                        class="post-author-photo"
-                        :src="post.author.photoURL"
-                        alt=""
+                            class="post-author-photo"
+                            :src="post.author.photoURL"
+                            alt=""
                     />
                 </div>
             </section>
+        </div>
         <div id="post">
 
         </div>
         <div id="comments">
-
-    </div>
+        </div>
     </main>
 </template>
 
 <style scoped lang="scss">
-    main {
-        display: flex;
-        flex-direction: row;
-        > div{
-            flex: 1 1 30%;
-            height: 100vh;
-            overflow-y: hidden;
-            -ms-overflow-style: none;  /* IE and Edge */
-            scrollbar-width: none;  /* Firefox */
-            border-right: white solid;
-            > header {
-                display: flex;
-                gap: 0.5rem;
-                padding: 0.5rem 1rem;
+main {
+  display: flex;
+  flex-direction: row;
 
-                > span {
-                    font-size: 1rem;
-                    letter-spacing: 2px;
-                    font-weight: 500;
-                    color: #aaaaaa;
-                }
+  > div {
+    flex: 1 1 30%;
+    height: 100vh;
+    overflow-y: hidden;
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+    border-right: white solid;
 
-            }
-        }
-        ::-webkit-scrollbar{
-            display: none;
-        }
+    > header {
+      display: flex;
+      gap: 0.5rem;
+      padding: 0.5rem 1rem;
+
+      > span {
+        font-size: 1rem;
+        letter-spacing: 2px;
+        font-weight: 500;
+        color: #aaaaaa;
+      }
+
     }
-    a {
-        background-color: $surfaceVariant;
-        color: rgba($onSurfaceVariant, 0.7);
-        border: none;
-        border-radius: 24px;
-        padding: 1rem;
-        text-decoration: none;
-        font-size: 0.8rem;
+  }
 
-        &:hover {
-            cursor: pointer;
-        }
-    }
+  ::-webkit-scrollbar {
+    display: none;
+  }
+}
 
-    #posts {
-        padding: 0 0.5rem 2rem 0.5rem;
-        overflow-y: auto;
-        display: flex;
-        flex-direction: column;
-        gap: 2rem;
-    }
+a {
+  background-color: $surfaceVariant;
+  color: rgba($onSurfaceVariant, 0.7);
+  border: none;
+  border-radius: 24px;
+  padding: 1rem;
+  text-decoration: none;
+  font-size: 0.8rem;
 
-    .post {
-        border-radius: 8px;
-        box-shadow: 0 0 10px 5px rgba(black, 0.1);
-        background-color: $surfaceVariant;
-        padding: 0.5rem 0.7rem;
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        overflow-wrap: break-word;
-    }
+  &:hover {
+    cursor: pointer;
+  }
+}
 
-    .post-header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+#posts {
+  padding: 0 0.5rem 2rem 0.5rem;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
 
-        > div {
-            font-size: 1.1rem;
-            font-weight: 500;
-            line-height: 1.4;
-        }
+.post {
+  border-radius: 8px;
+  box-shadow: 0 0 10px 5px rgba(black, 0.1);
+  background-color: $surfaceVariant;
+  padding: 0.5rem 0.7rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  overflow-wrap: break-word;
+}
 
-        > time {
-            font-size: 0.7rem;
-            line-height: 1.2;
-            opacity: 0.75;
-            text-align: end;
-            font-style: italic;
-        }
-    }
+.post-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-    .location-header {
-        display: flex;
-        align-items: center;
-        gap: 0.1rem;
+  > div {
+    font-size: 1.1rem;
+    font-weight: 500;
+    line-height: 1.4;
+  }
 
-        > span {
-            font-size: 0.6rem;
-            font-weight: 500;
-            align-content: center;
-        }
-    }
+  > time {
+    font-size: 0.7rem;
+    line-height: 1.2;
+    opacity: 0.75;
+    text-align: end;
+    font-style: italic;
+  }
+}
 
-    .post-image {
-        display: flex;
-        width: fit-content;
-        object-fit: cover;
-    }
-    .post-actions {
-        display: flex;
-        align-items: center;
-        gap: 0.3rem;
+.location-header {
+  display: flex;
+  align-items: center;
+  gap: 0.1rem;
 
-        .material-icons {
-            font-size: 20px;
-        }
+  > span {
+    font-size: 0.6rem;
+    font-weight: 500;
+    align-content: center;
+  }
+}
 
-        .post-author-username {
-            font-size: 0.8rem;
-            letter-spacing: 1px;
-        }
+.post-image {
+  display: flex;
+  width: fit-content;
+  object-fit: cover;
+}
 
-        .post-author-photo {
-            min-width: 24px;
-            max-width: 24px;
-            min-height: 24px;
-            max-height: 24px;
-            border-radius: 50%;
-        }
-    }
+.post-actions {
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+
+  .material-icons {
+    font-size: 20px;
+  }
+
+  .post-author-username {
+    font-size: 0.8rem;
+    letter-spacing: 1px;
+  }
+
+  .post-author-photo {
+    min-width: 24px;
+    max-width: 24px;
+    min-height: 24px;
+    max-height: 24px;
+    border-radius: 50%;
+  }
+}
 </style>
