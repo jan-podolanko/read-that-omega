835e198fd093fb2cbf570309bf6cb1aac4d361ec
jan-podolanko <jan.podolanko11@gmail.com>
2023-05-09 01:15:48 +0200

    sign in and sign up styling

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAXw4wF9kQY02acK8NIKvllgxKAFkjiWS0",
-    authDomain: "read-that-omega.firebaseapp.com",
-    projectId: "read-that-omega",
-    storageBucket: "read-that-omega.appspot.com",
-    messagingSenderId: "665544877435",
-    appId: "1:665544877435:web:786829fcf30ee63252c9de",
-    measurementId: "G-Y59LW4TY14"
+    apiKey: "AIzaSyC9OHFTJn8iBhRp-KUeKHU-jJiWvsjd0vk",
+    authDomain: "read-that-mobile.firebaseapp.com",
+    projectId: "read-that-mobile",
+    storageBucket: "read-that-mobile.appspot.com",
+    messagingSenderId: "1006517284776",
+    appId: "1:1006517284776:web:69b561df4c5d20a931379d",
+    measurementId: "G-T04ELDD0LH",
 };
